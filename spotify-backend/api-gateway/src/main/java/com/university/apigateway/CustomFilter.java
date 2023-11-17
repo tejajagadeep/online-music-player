034aab8e1594448ec9d46f80327bbf03e05f2f8a
Kaneti <2076212@cognizant.com>
2023-11-17 15:14:26 +0530

    shalem-commit

---
+++ spotify-backend/api-gateway/src/main/java/com/university/apigateway/CustomFilter.java
@@ -0,0 +1,28 @@
+package com.university.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.server.HttpServerResponse;
+
+@Configuration
+public class CustomFilter implements GlobalFilter {
+
+    Logger logger= LoggerFactory.getLogger(CustomFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request=exchange.getRequest();
+        logger.info("Authorization = " + request.getHeaders().get("Authorization"));
+        return chain.filter(exchange).then(Mono.fromRunnable(()->{
+            ServerHttpResponse response = exchange.getResponse();
+            logger.info(response.getStatusCode().toString());
+        }));
+    }
+}
