87f1ca45f8f78a93d7a8cd15ea14246967c6f320
tejajagadeep <teja.jagadeep@gmail.com>
2023-12-08 17:47:47 +0530

    changes made on  Fri Dec  8 17:47:46 IST 2023 by jagadeep

--- spotifyApp/src/app/navigation/side-bar/side-bar.component.ts
+++ spotifyApp/src/app/navigation/side-bar/side-bar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
+
+  username!: string;
+
+  ngOnInit(): void {
+    this.username = localStorage.getItem('authenticatedUser')+'';
+  }
 
 }
