17f260904123392021e4cf5ec83094d70b6b918a
tejajagadeep <teja.jagadeep@gmail.com>
2023-12-14 13:44:35 +0530

    changes made on  Thu Dec 14 13:44:35 IST 2023 by jagadeep

--- spotify-backend/music-service/src/test/java/com/spotify/musicservice/controller/SpotifyControllerTest.java
+++ spotify-backend/music-service/src/test/java/com/spotify/musicservice/controller/SpotifyControllerTest.java
@@ -1,32 +1,32 @@
-package com.spotify.musicservice.controller;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class SpotifyControllerTest {
-
-    @Test
-    void getSpotifyAccessToken() {
-    }
-
-    @Test
-    void getBillBoard100Playlist() {
-    }
-
-    @Test
-    void getTodayTopHitsPlaylist() {
-    }
-
-    @Test
-    void getDiscoverWeeklyPlaylist() {
-    }
-
-    @Test
-    void getTrack() {
-    }
-
-    @Test
-    void search() {
-    }
-}
+//package com.spotify.musicservice.controller;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class SpotifyControllerTest {
+//
+//    @Test
+//    void getSpotifyAccessToken() {
+//    }
+//
+//    @Test
+//    void getBillBoard100Playlist() {
+//    }
+//
+//    @Test
+//    void getTodayTopHitsPlaylist() {
+//    }
+//
+//    @Test
+//    void getDiscoverWeeklyPlaylist() {
+//    }
+//
+//    @Test
+//    void getTrack() {
+//    }
+//
+//    @Test
+//    void search() {
+//    }
+//}
---
+++ spotify-backend/wishlist-service/src/main/java/com/spotify/wishlistservice/controller/CustomController.java
@@ -0,0 +1,16 @@
+package com.spotify.wishlistservice.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class CustomController {
+
+    @GetMapping
+    public void custom(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+}
